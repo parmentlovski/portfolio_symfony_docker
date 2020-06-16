acfefd1cbca7c0020433261fb03acf4f18e4dc55
parmentlovski <parmentelot.bryan@hotmail.fr>
2020-06-16 14:21:31 +0000

    Envoie des informations relatives a la bdd sur la homepage

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use App\Repository\ProjetRepository;
+use App\Repository\ReseauxRepository;
+use App\Repository\ServiceRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminDashboardController extends AbstractController
 {
     /**
      * @Route("/dashboard", name="admin_dashboard")
      */
-    public function index()
+    public function index(ReseauxRepository $reseauxRepo, ServiceRepository $serviceRepo, UserRepository $userRepo, ProjetRepository $projetRepo)
     {
-        return $this->render('admin/dashboard/index.html.twig');
+
+        $reseaux = $reseauxRepo->findAll();
+        $services = $serviceRepo->findAll();
+        $users = $userRepo->findOneBy([], ['id' => 'asc']);
+        $projets = $projetRepo->findOneBy([], ['id' => 'desc']);
+
+        return $this->render('admin/dashboard/index.html.twig', [
+            'reseaux' => $reseaux,
+            'services' => $services,
+            'users' => $users,
+            'projets' => $projets
+        ]);
     }
 }
